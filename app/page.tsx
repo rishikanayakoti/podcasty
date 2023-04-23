9194cb65d5069049876b22a8546b33d00467ca71
Rishika Nayakoti <rishika.nayakoti3781@gmail.com>
2023-04-23 14:52:25 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 // TODO: Place your components in the respective
 // order
-
+import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div className="">
       {/* Header */}
+      <Navbar/>
       {/* Treding */}
+
       {/* Card Components */}
       {/* Footer */}
     </div>
---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import {MenuIcon, SearchIcon, MoonIcon, UserCircleIcon} from "@heroicons/react/solid";
+function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-3 md: px-10">
+      {/* left */}
+      <div className="flex">
+      <MenuIcon className =" h-6 px-3 cursor-pointer"/>    
+      <h1 className="cursor-pointer">APNACAST</h1>
+      </div>
+
+      {/* middle */}
+      <div className="flex border-2 items-center rounded-full py-1">
+       <input className=" flex-grow pl-5 bg-transparent outline-none" type="text" placeholder="search"/>
+       <SearchIcon className="h-6 px-2 text-black rounded-full cursor-pointer"/>
+      </div>
+
+      {/* right */}
+      <div className="flex justify-end items-center space-x-4">
+      <MoonIcon className="h-6 cursor-pointer"/>
+      <UserCircleIcon className=" h-6 cursor-pointer"/>
+      </div>
+    </header>
+    )
+}
+
+export default Navbar
