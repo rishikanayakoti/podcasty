e04a1a1516d0aadc4dabf3a232f39ce2b044fcd1
Rajender <rajenderkatkuri123@gmail.com>
2023-04-23 23:47:53 +0530

    Fetch podcasts from sanity

---
+++ components/PlaySVG.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PlaySVG = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="black-300"
+        className="w-6 h-6"
+      >
+        <path
+          className="fill-black-300"
+          stroke-linecap="round"
+          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default PlaySVG;
