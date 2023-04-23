0c36375555854bf8b56f9833eee3ae980c1cb386
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-23 16:11:24 +0530

    Frontend responsive done

---
+++ app/podcasts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PodcastsList from './PodcastsList';
+
+function Podcasts() {
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <PodcastsList />
+    </div>
+  )
+}
+
+export default Podcasts;
