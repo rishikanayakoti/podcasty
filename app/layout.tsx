ab8db9ebb4428b365647e39bbe3ff28b14a6856f
Rishika Nayakoti <rishika.nayakoti3781@gmail.com>
2023-04-23 14:41:55 +0530

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs/app-beta";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
